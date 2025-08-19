0f49a2eae5bc1a6a804a903b35e90dee1e1f9be2
natan <natanr123@gmail.com>
2025-08-19 12:12:42 +0300

    fdgdfgdfg

--- a.py
+++ a.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+from b import B
+
 class A:
     def hello(self):
         print("hello from A")
--- b.py
+++ b.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+from a import A
+
 class B:
     def hello(self):
         print("hello from B")
